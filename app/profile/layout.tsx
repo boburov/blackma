30a52a95b9411cf3c16f667b98233abd1e164d07
silvante <khamidov.ko@gmail.com>
2026-03-29 21:01:09 +0500

    Fonts

---
+++ app/profile/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
+import "../globals.css";
+
+const nunitoSans = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Blackma | Profile",
+  description: "kurier profile page",
+};
+
+export default function ProfileLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className={`${nunitoSans.variable} antialiased`}>{children}</div>;
+}
